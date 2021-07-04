b3b9e63b915f13f6be46291a4f0fd7c6cfb7defa
JORGE PATRICIO MARTINEZ MENDOZA <jpmartinez1@utpl.edu.ec>
2021-07-04 02:26:35 -0500

    feat: agrega visualizacion de rangos

---
+++ src/components/DetailRates.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="detail-container">
+    <div>
+      Costumer Type: {{ getCostumerTypeName(rate.customerType) }}
+    </div>
+    <div class="detail-rate">
+      <div>
+        Weekdays: &nbsp;
+      </div>
+      <div>
+        {{ rate.weekDay | currencyUSD }}
+      </div>
+    </div>
+    <div class="detail-rate">
+      <div>
+        Weekend: &nbsp;
+      </div>
+      <div>
+        {{ rate.weekend | currencyUSD }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { CostumerType } from '@/data/data';
+
+export default {
+  name: 'DetailRates',
+  filters: {
+    currencyUSD(value) {
+      return `$${value}`;
+    },
+  },
+  props: {
+    rate: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    getCostumerTypeName(costumerType) {
+      return CostumerType.filter((item) => item.id === costumerType)[0].name.toUpperCase();
+    },
+  },
+};
+</script>
+
+<style scoped>
+.detail-container {
+  padding: 3px;
+  margin: 1px;
+  border: 1px solid #EF9A9A;
+}
+.detail-rate {
+  justify-content: center;
+  display: flex;
+}
+</style>
---
+++ tests/unit/DetailRates.spec.js
@@ -0,0 +1,56 @@
+import { mount } from '@vue/test-utils';
+import App from '@/components/DetailRates.vue';
+import { Hotels } from '@/data/data';
+
+describe('Rate Details', () => {
+  it('Detail elements should has 2 elements of rates', async () => {
+    const wrapper = mount(App, {
+      propsData: {
+        rate: Hotels[0].rates[0],
+      },
+    });
+    const elements = wrapper.findAll('.detail-rate').length;
+    expect(elements).toBe(2);
+  });
+
+  it('Detail should has 1 elements', async () => {
+    const wrapper = mount(App, {
+      propsData: {
+        rate: Hotels[0].rates[0],
+      },
+    });
+    const elements = wrapper.findAll('.detail-container').length;
+    expect(elements).toBe(1);
+  });
+
+  it('Costumer Type should be regular', async () => {
+    const wrapper = mount(App, {
+      propsData: {
+        rate: Hotels[0].rates[0],
+      },
+    });
+    const uno = wrapper.vm.getCostumerTypeName(1);
+    expect(uno).toBe(('regular').toUpperCase());
+  });
+
+  it('Costumer Type shouldn\'t be regular', async () => {
+    const wrapper = mount(App, {
+      propsData: {
+        rate: Hotels[0].rates[0],
+      },
+    });
+    const uno = wrapper.vm.getCostumerTypeName(1);
+    expect(uno).toBe(('rewards').toUpperCase());
+  });
+
+  it('Detail should has format $###', async () => {
+    const wrapper = mount(App, {
+      propsData: {
+        rate: Hotels[0].rates[0],
+      },
+    });
+    const elements = wrapper.find('.detail-rate').text();
+    const valueExpected = `$${Hotels[0].rates[0].weekDay}`;
+    expect(elements).toContain(valueExpected);
+  });
+});
