ef5090bfc698f5850c40b39151d2dcafb4e33dc5
Patricio Martinez <jorge.martinez@tipti.market>
2021-11-22 13:01:27 -0500

    add: fingerprint feature

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,31 @@
 <template>
-  <div id="app">
+  <div id="app" style="height: 100%" >
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
+import FingerprintJS from '@fingerprintjs/fingerprintjs';
 import MainPage from './components/MainPage.vue';
 
 export default {
   name: 'App',
   components: {
     HelloWorld: MainPage,
+  },
+  data() {
+    return {
+      finger: '',
+    };
+  },
+  mounted() {
+    FingerprintJS.load()
+      .then((fp) => fp.get())
+      .then((result) => {
+        console.log(result);
+        this.finger = result.visitorId;
+      });
   },
 };
 </script>
